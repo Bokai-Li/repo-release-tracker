54c0610d1d7fc3f4635923dd696b42f2af6adcd0
Bokai Li <Bokai247@gmail.com>
2021-05-14 22:05:19 -0400

    Initial Commit

---
+++ GitHubReleaseTracker/GitHubReleaseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReleaseTrackerApp.swift
+//  GitHubReleaseTracker
+//
+//  Created by Bokai Li on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReleaseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
